4ed8e13ab41ee9dd5955dee019f50ebdc875f09f
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-02-20 01:09:09 -0600

    commented out first 2 exercises

--- problem-1-multiples-of-3-and-5.js
+++ problem-1-multiples-of-3-and-5.js
@@ -3,11 +3,14 @@
 
 function multiplesOf3and5(number) {
   let sum = 0;
+  //run a loop up to the input number
   for (let i = 0; i < number; i++) {
+    //if divisible by 3 or 5, add it to the sum
     if (i % 3 === 0 || i % 5 === 0) {
       sum += i;
     }
-  } 
+  }
+  //return the calculated sum
   return sum;
 }
 
--- problem-2-even-fibonacci-numbers.js
+++ problem-2-even-fibonacci-numbers.js
@@ -2,17 +2,22 @@
 // find the sum of the even-valued terms.
 
 function fiboEvenSum(n) {
+  //initialize and set variables to calculate fibo nums and calculated sum
   let prev = 0;
   let curr = 1;
   let temp;
   let sum = 0;
+  //run a loop as long as current fibo num is less than the input
   while (curr < n) {
+    //implement fibonacci formula to find each iteration
     temp = curr;
     curr = curr + prev;
     prev = temp;
+    //if the iteration is divisible by 2, add it to sum
     if (curr % 2 === 0) {
       sum += curr;
     }
   }
+  //return calculated sum
   return sum;
 }
