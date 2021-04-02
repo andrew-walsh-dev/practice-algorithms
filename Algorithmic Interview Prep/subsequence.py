3b5cff4a0bff3098201e200a03e2c30527c58ca0
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-04-01 22:16:06 -0500

    setup subsequnce algo problem

---
+++ Algorithmic Interview Prep/subsequence.py
@@ -0,0 +1,2 @@
+#Given two arrays, write a function that determines whether the second array is a subsequence of the first one
+#NOTE that the elements must appear in order but DO NOT have to be adjacent in the array
