1df46c87ff491b9c56f352b6d386ae0bd2a5857d
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-04-03 14:19:02 -0500

    added solution and comments for closest BST

--- Algorithmic Interview Prep/closestValueInBST.py
+++ Algorithmic Interview Prep/closestValueInBST.py
@@ -3,6 +3,29 @@
 #that target contained in the BST
 #You can assume there will be only one closest value
 
+#call our recursive helper function to traverse the tree
+#and find our value
+def findClosestInBST(tree, target):
+    return findClosest(tree, target, float("inf"))
+
+def findClosest(tree, target, closest):
+    #if hit bottom of tree, return the closest we found 
+    if tree is None:
+        return closest
+    #if the value we just found is closer than previous closest, 
+    #update closest
+    if abs(tree.value - target) < abs(closest - target):
+        closest = tree.value
+    #if target is greater than current value, go right in BST
+    if target > tree.value:
+        return findClosest(tree.right, target, closest)
+    #if less, go left in the BST
+    elif target < tree.value:
+        return findClosest(tree.left, target, closest)
+    #if current value = target, return it, we are done
+    else: 
+        return closest
+
 class BST:
     def __init__(self, value):
         self.value = value
