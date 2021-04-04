69fab39dc3f142a888c595dd29bb26b0a0097bff
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-04-04 13:00:51 -0500

    added solution and comments for binary tree sums

--- Algorithmic Interview Prep/binaryTreeSums.py
+++ Algorithmic Interview Prep/binaryTreeSums.py
@@ -8,3 +8,30 @@ class BinaryTree:
         self.value = value
         self.left = None
         self.right = None
+
+
+#main function
+#makes list of sums to add to 
+#and calls recursive helper function
+def get_branch_sums(root):
+    sums = []
+    calculate_sums(root, 0, sums)
+    return sums
+
+#used recursion to traverse and calculate branches
+#using depth-first search
+def calculate_sums(node, runningSum, sums):
+    #if node is none, we are done with that branch
+    if node is None:
+        return
+
+    #else add curr value to running branch sum
+    runningSum += node.value
+    #if no children, add branches sum to list and return
+    if node.left is None and node.right is None:
+        sums.append(runningSum)
+        return
+
+    #else we call on each child and pass down the running sum
+    calculate_sums(node.left, runningSum, sums)
+    calculate_sums(node.right, runningSum, sums)
