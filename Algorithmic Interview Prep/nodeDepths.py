c322c6341dfd302085114ae8819264e2fcdbd30a
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-04-04 20:31:23 -0500

    added solution and comments for node depths problem

--- Algorithmic Interview Prep/nodeDepths.py
+++ Algorithmic Interview Prep/nodeDepths.py
@@ -7,3 +7,12 @@ class BinaryTree:
         self.left = None
         self.right = None
 
+#very elegant recursive solution
+#each node tells its children their depth (by adding 1 to its own depth)
+#and the recrusive calls traverse the tree until the leaves and sum 
+#up all of the depths 
+def nodeDepths(node, depth=0):
+    if node is None:
+        return 0
+    else:
+        return depth + nodeDepths(node.left, depth + 1) + nodeDepths(node.right, depth + 1)
