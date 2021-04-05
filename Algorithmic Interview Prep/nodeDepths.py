326f6053e663179988d4cf8d655f411b72897ef0
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-04-04 20:28:10 -0500

    added binary tree node depths problem

---
+++ Algorithmic Interview Prep/nodeDepths.py
@@ -0,0 +1,9 @@
+#write a function that takes in a binary tree and 
+#returns a sum of its node's depths
+
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
