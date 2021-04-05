dcd91083317cad7b89ccb0b546425104dc51b992
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-04-05 09:55:45 -0500

    added depth first method and boilerplate node class

---
+++ Algorithmic Interview Prep/depthFirstSearch.py
@@ -0,0 +1,15 @@
+#Given a Node class that has a name and an array of optional children nodes, 
+#implement depth first seach method on the Node class
+
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        # Write your code here.
+        pass
