ce5fe94fb244e72ecd69e885cabadefa7410ef14
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-04-18 23:51:41 -0500

    finished palindrome

--- Algorithmic Interview Prep/palindrome_check.py
+++ Algorithmic Interview Prep/palindrome_check.py
@@ -1,3 +1,6 @@
 #Write a function that takes in a string and returns a boolean value
 #based on the string being a palindrome
 
+def isPalindrome(string):
+	return string[::-1] == string
+#easy one liner using python's string slicing syntax
